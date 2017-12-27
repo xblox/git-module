502f646b2cadfc65f66ae630c615eabf9c20161f
xblox <asd28071977>
2017-12-27 19:17:06 +0100

    =save only

---
+++ src/commands/commit.ts
@@ -0,0 +1,32 @@
+import * as debug from '../debug';
+import * as chalk from 'chalk';
+import * as CLI from 'yargs';
+import { defaultOptions, sanitize } from '../argv';
+import { each } from '../lib';
+import { get } from '../modules';
+import { Module } from '../module';
+import { IEachOptions } from '../types';
+import * as dir from '@xblox/fs/dir';
+const options = (yargs: CLI.Argv) => defaultOptions(yargs.option('command', {
+    describe: 'the command to run per module'
+}).option('message', { describe: 'the commit message' }));
+
+const description = (): string => {
+    return 'Run a command for all modules' +
+        '\n\t Parameters : ' +
+        chalk.green('\n\t\t\t --command=[command to run]');
+};
+export const register = (cli: CLI.Argv) => {
+    return cli.command('list', description(), options, (argv: CLI.Arguments) => {
+        if (argv.help) { return; }
+        const args = sanitize(argv) as IEachOptions;
+        if (args.target) {
+            dir.sync(args.target);
+        }
+        args.command = 'commit';
+        const modules: Module[] = get(args.source, args.target, args.profile).map((module) => module.pack());
+        debug.inspect('modules:', modules);
+        // const all = each(modules, args, ['commit', '--message=' + argv.message]);
+        // all.then((r) => debug.inspect('test', r));
+    });
+};
---
+++ src/commands/update.ts
@@ -0,0 +1,33 @@
+import * as debug from '../debug';
+import * as chalk from 'chalk';
+import * as CLI from 'yargs';
+import { defaultOptions, sanitize } from '../argv';
+import { each } from '../lib';
+import { get } from '../modules';
+import { IEachOptions } from '../types';
+import * as dir from '@xblox/fs/dir';
+const options = (yargs: CLI.Argv) => defaultOptions(yargs.option('command', {
+    describe: 'the command to run per module'
+}));
+
+const description = (): string => {
+    return 'Run a command for all modules' +
+        '\n\t Parameters : ' +
+        chalk.green('\n\t\t\t --command=[command to run]');
+};
+export const register = (cli: CLI.Argv) => {
+    return cli.command('update', description(), options, (argv: CLI.Arguments) => {
+        if (argv.help) { return; }
+        const args = sanitize(argv) as IEachOptions;
+        if (args.target) {
+            dir.sync(args.target);
+        }
+        args.command = 'pull';
+        if (args.verbose) {
+            debug.info('Update modules');
+        }
+        const modules = get(args.source, args.target, args.profile);
+        const all = each(modules, args, []);
+        all.then((r) => debug.inspect('Update', r));
+    });
+};
